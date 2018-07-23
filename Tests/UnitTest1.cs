5b00500b7fdf760052494b54c1aa134c4fe6ef2c
Shoaib Manzoor <shoaibbwsd@gmail.com>
2018-07-23 12:34:10 +0500

    Login Window test

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,14 +1,37 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems.TabItems;
 
 namespace Tests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private TestStack.White.Application application;
+        
+        public void SetupApp()
         {
+            string outputDir = @"C:\Users\Shoaib\Desktop\HospitalScheduling\HospitalScheduling\bin\Debug\HospitalScheduling.exe";
+
+            application = TestStack.White.Application.Launch(outputDir);
+            //Window mainWindow = application.GetWindow("LoginWindow");
+        }
+
+        [TestMethod]
+        public void LoginWindow_Test()
+        {
+            SetupApp();
+            Window mainWindow = application.GetWindow("Login Window");
+            Button loginBtn = mainWindow.Get<Button>(SearchCriteria.ByText("OK"));
+            loginBtn.Click();
+
+            Console.Write("Logged in");
         }
     }
 }
